4325a16fc8a2b106f92f057af490cc5bbf6f1b2a
nabil19911 <nabilshaahani@gmail.com>
2025-01-30 22:59:27 +0530

    initial commit

--- routes/outlet.route.js
+++ routes/outlet.route.js
@@ -1,37 +1,5 @@
 import { Router } from "express";
-import roles from "../constant/roles.js";
-
-import {
-  createNewOutlet,
-  getAllOutlets,
-  getOutletGasRequests,
-  requestGas,
-} from "../controllers/outlet.controller.js";
-import authenticate from "../middleware/auth.js";
 
 const router = Router();
-
-// get all outlets
-router.get(
-  "/",
-  authenticate([
-    roles.ADMIN,
-    roles.CUSTOMER,
-    roles.BRANCH_MANAGER,
-    roles.DISPATCH_OFFICER,
-  ]),
-  getAllOutlets
-);
-
-// create outlet
-router.post("/create/", authenticate([roles.ADMIN]), createNewOutlet);
-
-router.post("/gas-request/", authenticate([roles.BRANCH_MANAGER]), requestGas);
-
-router.get(
-  "/gas-request/:id",
-  authenticate([roles.BRANCH_MANAGER]),
-  getOutletGasRequests
-);
 
 export default router;
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,47 +1,5 @@
 import { Router } from "express";
-import roles from "../constant/roles.js";
-import {
-  createNewUser,
-  getAllUsers,
-  getUserByEmail,
-  getUserById,
-  getUserProfile,
-  updateUserById,
-} from "../controllers/user.controller.js";
-import authenticate from "../middleware/auth.js";
-import { upload } from "../middleware/upload.js";
 
 const router = Router();
-
-// Get all user
-router.get("/", authenticate([roles.ADMIN]), getAllUsers);
-
-// get user by id
-router.get("/:id", authenticate([roles.ADMIN]), getUserById);
-
-// update user by id
-router.patch("/:id", authenticate([roles.ADMIN]), updateUserById);
-
-// Default route
-router.post("/", authenticate(roles.ADMIN), getUserByEmail);
-
-// Get customer profile route
-router.post(
-  "/profile/",
-  authenticate([
-    roles.ADMIN,
-    roles.CUSTOMER,
-    roles.BRANCH_MANAGER,
-    roles.DISPATCH_OFFICER,
-  ]),
-  getUserProfile
-);
-
-router.post(
-  "/create/",
-  authenticate([roles.ADMIN, roles.BRANCH_MANAGER]),
-  upload.single("business_registration_certification_path"),
-  createNewUser
-);
 
 export default router;
